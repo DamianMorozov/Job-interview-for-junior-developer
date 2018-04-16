b23929fb6fa97ca9843cedd125058564b7668ad5
Morozov Damian <web@morozov33.ru>
2018-04-16 21:46:28 +0300

    [16.04.2018]
    [+] Добавлен вопрос 1.
    [+] Добавлен вопрос 2.

---
+++ Question_01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Question_01
+{
+    class A
+    {
+        public virtual void Foo()
+        {
+            Console.WriteLine("Class A");
+        }
+    }
+
+    class B : A
+    {
+        public override void Foo()
+        {
+            Console.WriteLine("Class B");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            A obj1 = new A();
+            obj1.Foo();
+
+            B obj2 = new B();
+            obj2.Foo();
+
+            A obj3 = new B();
+            obj3.Foo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Question_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Question_02
+{
+    internal class Program
+    {
+        public struct S : IDisposable
+        {
+            private bool dispose;
+            public void Dispose()
+            {
+                dispose = true;
+            }
+            public bool GetDispose()
+            {
+                return dispose;
+            }
+        }
+
+        private static void Main()
+        {
+            var s = new S();
+            using (s)
+            {
+                Console.WriteLine(s.GetDispose());
+            }
+            Console.WriteLine(s.GetDispose());
+
+            Console.ReadKey();
+        }
+    }
+}
