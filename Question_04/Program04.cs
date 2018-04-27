f4ae7a5df9c8803a5509169a2b1babfd490193d2
Morozov Damian <web@morozov33.ru>
2018-04-27 23:38:07 +0300

    [27.04.2018]
    [+] Добавлен вопрос 7.

---
+++ Question_04/Program04.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Question_04
+{
+    class Program04
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            object obj = i;
+            ++i;
+            Console.WriteLine(i);
+            Console.WriteLine(obj);
+            Console.WriteLine((short)obj);
+
+            Console.ReadKey();
+        }
+    }
+}
