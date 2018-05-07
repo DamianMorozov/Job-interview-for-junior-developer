1d2ca2867e28d514e2feed62f8b21b9a923e17c0
Morozov Damian <web@morozov33.ru>
2018-05-07 09:44:51 +0300

    [07.05.2018]
    [+] Добавлен вопрос 5.
    [+] Добавлен вопрос 6.

---
+++ Question_05/Program05.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Question_05
+{
+    class Program05
+    {
+        static void Main(string[] args)
+        {
+            var s1 = string.Format("{0}{1}", "abc", "cba");
+            var s2 = "abc" + "cba";
+            var s3 = "abccba";
+
+            Console.WriteLine(s1 == s2);
+            Console.WriteLine((object)s1 == (object)s2);
+            Console.WriteLine(s2 == s3);
+            Console.WriteLine((object)s2 == (object)s3);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Question_06/Program06.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Question_06
+{
+    internal class Program
+    {
+        private static Object syncObject = new Object();
+
+        private static void Write()
+        {
+            lock (syncObject)
+            {
+                Console.WriteLine("test");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            lock (syncObject)
+            {
+                Write();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
