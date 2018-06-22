db4d2a6d200677c777f1620e9051bf63f16641e8
Morozov Damian <web@morozov33.ru>
2018-06-22 17:06:23 +0300

    [22.06.2018]
    [+] Добавлен вопрос 7.

--- Question_01/Program01.cs
+++ Question_01/Program01.cs
@@ -2,54 +2,40 @@
 
 namespace Question_01
 {
-    class A
+    internal class A
     {
         public virtual void Foo()
         {
             Console.WriteLine("Class A");
         }
-
-        //public void SomeMethodA()
-        //{
-        //    Console.WriteLine("SomeMethod A");
-        //}
     }
 
-    class B : A
+    internal class B : A
     {
         public override void Foo()
         {
             Console.WriteLine("Class B");
         }
-
-        //public new void SomeMethodA()
-        //{
-        //    Console.WriteLine("new SomeMethod A");
-        //}
-
-        //public void SomeMethodB()
-        //{
-        //    Console.WriteLine("SomeMethod B");
-        //}
     }
 
     internal class Program01
     {
         private static void Main()
         {
+            //B obj1 = new A();  // ошибка
             A obj1 = new A();
             obj1.Foo();
 
             B obj2 = new B();
             obj2.Foo();
-            //obj2.SomeMethodA();
 
             A obj3 = new B();
             obj3.Foo();
-            //obj3.SomeMethodA();
+
+            B obj4 = new B();
+            ((A)obj4).Foo();
 
             Console.ReadKey();
         }
-
     }
 }
---
+++ Question_07/Program07.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Question_07
+{
+    internal class Program07
+    {
+        public class A
+        {
+            public virtual void Print1()
+            {
+                Console.Write("A");
+            }
+            public void Print2()
+            {
+                Console.Write("A");
+            }
+        }
+
+        public class B : A
+        {
+            public override void Print1()
+            {
+                Console.Write("B");
+            }
+        }
+
+        public class C : B
+        {
+            public new void Print2()
+            {
+                Console.Write("C");
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            var c = new C();
+            A a = c;
+
+            a.Print2();
+            a.Print1();
+            c.Print2();
+
+            Console.ReadKey();
+        }
+    }
+}
